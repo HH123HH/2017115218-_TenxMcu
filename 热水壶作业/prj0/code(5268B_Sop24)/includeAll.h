bbae6680f6d78de4f4f22106c9e15943e32810c6
HH123HH <1483204856@qq.com>
2020-01-06 17:43:44 +0800

    头文件以及主函数的编写

---
+++ 热水壶作业/prj0/code(5268B_Sop24)/includeAll.h
@@ -0,0 +1,11 @@
+#include "REGtenxTM52F5268b.h"
+#include "typeAlias.h"
+#include <INTRINS.H>
+
+#include "buzz.h"
+#include "display.h"
+#include "init.h"
+#include "isr.h"
+#include "keys.h"
+#include "main.h"
+#include "tempAd.h"
